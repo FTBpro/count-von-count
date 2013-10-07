927d8b06df958ee2b2d33fbb1d63c61674ac9b17
Ronsho <ron@ftbpro.com>
2013-10-07 13:04:07 +0200

    some basic changes

--- spec/lib/action_counter_spec.rb
+++ spec/lib/action_counter_spec.rb
@@ -1,16 +1,13 @@
 require 'spec_helper'
-
+require 'script_loader'
 describe "ActionCounter" do
-  before :all do
-    `ruby lib/load_nginx_lua_script.rb`
-  end
   before do
-    @old_reads_count = Integer(`redis-cli get 'post_reads_131'`)
-    `curl "http://localhost:8090/reads?post_id=131&user_slug=shai.k"`
+    ScriptLoader.load
   end
   context "post reads" do
     it "should increment post reads by 1" do
-      Integer(`redis-cli get 'post_reads_131'`).should eq (@old_reads_count + 1)
+      expect { `curl "http://localhost:8090/reads?post_id=131&user_slug=shai.k"`}.to change { Integer(`redis-cli get 'post_reads_131'`)}.by(1)
+      # Integer(`redis-cli get 'post_reads_131'`).should eq (@old_reads_count + 1)
     end
   end
 end

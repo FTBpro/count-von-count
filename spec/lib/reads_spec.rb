df3d639d8387546b1b0ba7e94eeae0ec9b045f5e
Ronsho <ron@ftbpro.com>
2013-10-21 14:22:14 +0300

    Added locale to leader board

--- spec/lib/reads_spec.rb
+++ spec/lib/reads_spec.rb
@@ -11,10 +11,10 @@ describe "Read Action" do
     @league_monthly = create :LeagueMonthly, { league: league_id, locale: @locale, month: Time.now.strftime("%m"), year: Time.now.strftime("%Y") }
     @team_weekly = create :TeamWeekly, { team: team_id, locale: @locale, week: Time.now.strftime("%W"), year: Time.now.strftime("%Y") }
     @team_monthly = create :TeamMonthly, { team: team_id, locale: @locale, month: Time.now.strftime("%m"), year: Time.now.strftime("%Y") }
-    @league_weekly_leaderboard = create :LeagueWeeklyLeaderboard, { league: league_id, week: Time.now.strftime("%W"), year: Time.now.strftime("%Y") }
-    @league_monthly_leaderboard = create :LeagueMonthlyLeaderboard, { league: league_id, month: Time.now.strftime("%m"), year: Time.now.strftime("%Y") }
-    @team_weekly_leaderboard = create :TeamWeeklyLeaderboard, { team: team_id, week: Time.now.strftime("%W"), year: Time.now.strftime("%Y") }
-    @team_monthly_leaderboard = create :TeamMonthlyLeaderboard, { team: team_id, month: Time.now.strftime("%m"), year: Time.now.strftime("%Y") }
+    @league_weekly_leaderboard = create :LeagueWeeklyLeaderboard, { league: league_id, locale: @locale, week: Time.now.strftime("%W"), year: Time.now.strftime("%Y") }
+    @league_monthly_leaderboard = create :LeagueMonthlyLeaderboard, { league: league_id, locale: @locale, month: Time.now.strftime("%m"), year: Time.now.strftime("%Y") }
+    @team_weekly_leaderboard = create :TeamWeeklyLeaderboard, { team: team_id, locale: @locale, week: Time.now.strftime("%W"), year: Time.now.strftime("%Y") }
+    @team_monthly_leaderboard = create :TeamMonthlyLeaderboard, { team: team_id, locale: @locale, month: Time.now.strftime("%m"), year: Time.now.strftime("%Y") }
   end
 
   before :all do

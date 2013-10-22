67eb876f8744682eddc2d29d893ebed8b0f7e0e2
Ronsho <ron@ftbpro.com>
2013-10-22 15:46:23 +0300

    updated the get method

--- lib/get.lua
+++ lib/get.lua
@@ -5,7 +5,15 @@ red:set_timeout(1000) -- 1 sec
 red:connect("127.0.0.1", 6379)
 
 local cjson = require "cjson"
-local value = red:hgetall(args["key"])
-local hash = red:array_to_hash(value)
-local response = cjson.encode(hash)
+local key = args["key"]
+
+local response = ""
+if red:type(key) == "hash" then
+  local value = red:hgetall(key)
+  local hash = red:array_to_hash(value)
+  response = cjson.encode(hash)
+else
+  local value = red:zrevrange(key, 0, -1, "withscores")
+  response = cjson.encode(value)
+end
 ngx.say(response)

0a3dc8e1e3dad6f41258a7a716d17bf126ee8d67
Ronsho <ron@ftbpro.com>
2013-10-28 13:13:50 +0200

    update geo ip

--- lib/scripts/update-geoip.sh
+++ lib/scripts/update-geoip.sh
@@ -2,4 +2,4 @@
 wget -N -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
 gunzip -f GeoIP.dat.gz
 mv GeoIP.dat /usr/share/GeoIP/GeoIP.dat
-rm GeoIP.dat.gz
+nginx -s reload

3feeb9405a9f8a4d573362d265b80f7e0c1a1c15
Ronsho <ron@ftbpro.com>
2013-11-28 11:36:56 +0200

    fixed s3 monitor

--- lib/scripts/monitors/s3-monitor.sh
+++ lib/scripts/monitors/s3-monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 BUCKET_NAME=$1
 FOLDER_TO_CHECK=$(date +%Y/%m/%d -d "yesterday")
-RESPONSE=$(aws s3 ls s3://$BUCKET_NAME/@FOLDER_TO_CHECK)
+RESPONSE=$(aws s3 ls s3://$BUCKET_NAME/$FOLDER_TO_CHECK)
 
 if echo "$RESPONSE" | grep ".gz"; then
-  echo "matched";
+  echo "matched"
 else
   echo "No S3 backups for $FOLDER_TO_CHECK." | mail -s "Action Counter Monitor" ron@ftbpro.com
 fi

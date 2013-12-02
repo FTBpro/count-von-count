d27ceb8a4f06322af8b508a85ec90f515c347d13
Shai Kerer <shai@ftbpro.com>
2013-12-02 17:00:12 +0200

    removed mobile left overs

--- lib/init.lua
+++ lib/init.lua
@@ -1,8 +1,10 @@
 function initGeoIP() 
 	local geoip = require "geoip";
 	local geoip_country = require "geoip.country";
-	local geoip_file = "/usr/share/GeoIP/GeoIP.dat"
+	local geoip_file = "/path/to/geoip.dat"
 	local geoip_country_filename = geoip_file
 	geodb = geoip_country.open(geoip_country_filename)
 end
 
+-- Uncomment this line if you want to use the geoIP, after you followed the installation instructions
+-- initGeoIP()

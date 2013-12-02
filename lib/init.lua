d45525d358c61bd8e66833d1b0c9c935f029da80
Shai Kerer <shai@ftbpro.com>
2013-12-02 16:26:18 +0200

    cleanups for opensource

--- lib/init.lua
+++ lib/init.lua
@@ -1,5 +1,8 @@
-local geoip = require "geoip";
-local geoip_country = require "geoip.country";
-local geoip_file = "/usr/share/GeoIP/GeoIP.dat"
-local geoip_country_filename = geoip_file
-geodb = geoip_country.open(geoip_country_filename)
+function initGeoIP() 
+	local geoip = require "geoip";
+	local geoip_country = require "geoip.country";
+	local geoip_file = "/usr/share/GeoIP/GeoIP.dat"
+	local geoip_country_filename = geoip_file
+	geodb = geoip_country.open(geoip_country_filename)
+end
+

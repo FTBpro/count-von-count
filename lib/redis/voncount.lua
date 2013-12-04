4f784635da0ca1ada073894e9d771a56ec285f03
Shai Kerer <shai@ftbpro.com>
2013-12-04 14:49:36 +0200

    small fix for non existing variables

--- lib/redis/voncount.lua
+++ lib/redis/voncount.lua
@@ -175,7 +175,11 @@ local function addValuesToKey(tbl, key)
        concatToArray(tempResult, dup)
     end
     rslt = tempResult
-    match = rslt[1]:match("{[%w_]*}")
+    if #rslt > 0 then
+      match = rslt[1]:match("{[%w_]*}")
+    else
+      match = nil
+    end
   end
 
   if #rslt == 1 then

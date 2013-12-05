8ab50c098de932b695f183a128f0b57128d8c6ec
Shai Kerer <shai@ftbpro.com>
2013-12-05 14:06:10 +0200

    added PATH setting to setup.sh

--- lib/scripts/setup.sh
+++ lib/scripts/setup.sh
@@ -1,11 +1,13 @@
 DEPLOY_TO='/home/deploy/count-von-count/current'
 NGINX_DIR="/usr/local/openresty/nginx"
 USER=$(whoami)
+PATH=$NGINX_DIR/sbin:$PATH
 
 chown -R $USER:$USER $NGINX_DIR
-ln -sf $DEPLOY_TO/current/ $NGINX_DIR/count-von-count
+export PATH
+ln -sf $DEPLOY_TO/ $NGINX_DIR/count-von-count
 mkdir -p $NGINX_DIR/conf/include
-ln -sf $DEPLOY_TO/current/config/voncount.nginx.conf $NGINX_DIR/conf/include/voncount.conf
+ln -sf $DEPLOY_TO/config/voncount.nginx.conf $NGINX_DIR/conf/include/voncount.conf
 service redis-server start
 nginx
 

abb7b3f0806882ae19bf94494221f6964d43bd74
Shai Kerer <shai@ftbpro.com>
2013-12-05 15:05:49 +0200

    fixed deploy shell scripts

--- lib/scripts/setup.sh
+++ lib/scripts/setup.sh
@@ -1,13 +1,12 @@
 DEPLOY_TO='/home/deploy/count-von-count/current'
 NGINX_DIR="/usr/local/openresty/nginx"
 USER=$(whoami)
-PATH=$NGINX_DIR/sbin:$PATH
 
 chown -R $USER:$USER $NGINX_DIR
-export PATH
 ln -sf $DEPLOY_TO/ $NGINX_DIR/count-von-count
 mkdir -p $NGINX_DIR/conf/include
 ln -sf $DEPLOY_TO/config/voncount.nginx.conf $NGINX_DIR/conf/include/voncount.conf
 service redis-server start
-nginx
+./reload.sh
+$NGINX_DIR/sbin/nginx
 
